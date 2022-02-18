641ce644cc4c3065e2d05fdcc1960aecf17bc0c4
André Ciappina <91566252+DexREAU@users.noreply.github.com>
2022-02-18 16:20:18 -0300

    improvements

--- script.js
+++ script.js
@@ -1,53 +1,4 @@
 
-ScrollReveal().reveal('body, h1, .underdog_sec article h2, .underdog_sec article h3, .why_sec span, .why_sec p, .web3_sec p, .how_sec p, .products_sec h6, .products_sec p, .products_list li, .products_list img, .blog_section article', {
-    duration:1200,
-    easing: 'ease',
-});
-
-const timelineCtnr = document.querySelector('.timeline_ctnr')
-
-let startWindowsPosition;
-let windowPosition;
-
-let scrollTime;
-
-if (window.matchMedia("(orientation:landscape)").matches) {
-    ScrollReveal().reveal(timelineCtnr, {
-        duration:0,
-        easing: 'ease',
-        afterReveal: function (el) {
-            startWindowsPosition = window.pageYOffset;
-    
-            scrollTime = true;
-            
-            document.body.onscroll = function() {
-                if(scrollTime == false) {
-    
-                } else {
-                    windowPosition = window.pageYOffset;
-                
-                    timelineCtnr.scrollLeft = (timelineCtnr.scrollLeft) + (windowPosition - startWindowsPosition)/50;
-    
-                    setTimeout(() => {
-                        scrollTime = false;
-                    }, 3000);
-                }
-            }
-        }
-    });
-}
-
-
-
-
-document.onreadystatechange = function () {
-    if (document.readyState == "interactive" || document.readyState == "complete") {
-        setTimeout(() => {
-            menuBt.style.transform = 'translateY(0) rotate(0deg)'
-        }, 200);
-    }
-  }
-
 const menu = document.querySelector('menu')
 
 const menuAnchors = document.querySelectorAll('menu a')
@@ -102,6 +53,62 @@ document.body.onscroll = function() {
         hamburguer()
     }
 }
+
+
+const loadingBg = document.getElementById('loading_bg')
+
+document.onreadystatechange = function () {
+    if (document.readyState == "interactive" || document.readyState == "complete") {
+        loadingBg.style.opacity = 0;
+        loadingBg.style.visibility = 'hidden';
+        setTimeout(() => {
+            menuBt.style.transform = 'translateY(0) rotate(0deg)'
+        }, 200);
+    }
+}
+
+
+
+
+ScrollReveal().reveal('h1, .underdog_sec article h2, .underdog_sec article h3, .why_sec span, .why_sec p, .web3_sec p, .how_sec p, .products_sec h6, .products_sec p, .products_list li, .products_list img, .blog_section article', {
+    duration:1200,
+    easing: 'ease',
+});
+
+const timelineCtnr = document.querySelector('.timeline_ctnr')
+
+let startWindowsPosition;
+let windowPosition;
+
+let scrollTime;
+
+if (window.matchMedia("(orientation:landscape)").matches) {
+    ScrollReveal().reveal(timelineCtnr, {
+        duration:0,
+        easing: 'ease',
+        afterReveal: function (el) {
+            startWindowsPosition = window.pageYOffset;
+    
+            scrollTime = true;
+            
+            document.body.onscroll = function() {
+                if(scrollTime == false) {
+    
+                } else {
+                    windowPosition = window.pageYOffset;
+                
+                    timelineCtnr.scrollLeft = (timelineCtnr.scrollLeft) + (windowPosition - startWindowsPosition)/50;
+    
+                    setTimeout(() => {
+                        scrollTime = false;
+                    }, 3000);
+                }
+            }
+        }
+    });
+}
+
+
 
 
 
